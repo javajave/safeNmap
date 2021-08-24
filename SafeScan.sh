2c9818ca0a5f6107c17fd0cb4d3b96d749c4289b
javajave <71944191+javajave@users.noreply.github.com>
2021-08-24 15:47:06 +0300

    Update SafeScan.sh

--- SafeScan.sh
+++ SafeScan.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# This script
-# Ask if you want to be anonymous, verify if nipe installed, and if not, install it.
-# Give you the option to scan the local network, or an external address.
-# Creates a list of the entire address range of the target IP.
-# Randomly scan an IP and a port address, if it's open, the results will be saved with timestamp in .log file.
-
 function SCAN() { #Scans randomly and keeps only ports open with a time stamp
 for ip in $(cat MixIP.list)													# Random ip address
 do
